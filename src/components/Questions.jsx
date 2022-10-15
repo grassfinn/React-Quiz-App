7d0ec28920bac1ef03b48c3d3ec42955fcf12cd1
grassfinn <theharper@live.com>
2022-10-14 22:15:57 -0400

    Validate answers

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,50 +2,89 @@ import React from "react";
 import he from 'he'
 
 export default function Questions(props){
-    
-    
-    const [submitted, setSubmitted] = React.useState(false)
-    const questions = props.questions
-    const answers = props.answers
-    console.log(props)
 
-       function validate(e){
-        e.preventDefault()
-        document.querySelectorAll('.question-answer').forEach(answer => {
-            
-            // console.log(answer.getAttribute('data-answer'))
-            // if (answer.data-answer === 'true'){
-            //     answer.setAttribute('class', 'green')
-            // }
-        })
-        setSubmitted(prevState => !prevState)
-        // if (answers.question1 === 'true'){
-            
+
+    
+    
+    // const validation = {
+        //     backgroundColor: ' green'
         // }
-   
-    }
+        const [checkAnswer, setCheckAnswer] =React.useState({})
+        const [submitted, setSubmitted] = React.useState(false)
+        const [points, setPoints] = React.useState(0)
+        const questions = props.questions
+        const answers = props.answers
+        React.useEffect(() => {
+        },[checkAnswer])
+        
+        React.useEffect(() => {
+            console.log('calling',props.apiCall)
+            console.log(questions)
+        },[submitted])
+        function validate(e){
+            const validateQuestion = Object.entries(checkAnswer)
+            setPoints(0)
+            if (!submitted) {
+                e.preventDefault()
 
-    const renderQuestions = questions.map((item,questionIndex) => {
+            }
+            
+            // check to see if they havent all be answered
+            if (questions.length > validateQuestion.length) {
+                alert('you havent answer them all')
+                setSubmitted(false)
+                return
+            }
+            
+            // checking the obj and returning the key value pairs of it in an array
+            // looping through he objects
+                for (const entry of validateQuestion) {
+                    const [userQuestion, userAnswer] = entry
+                    // find the obj.question that is equal to the userQuestion
+                    const matchedQuestion = questions.find(obj => obj.question === userQuestion)
+                   if (userAnswer === matchedQuestion.correct_answer){
+                        setPoints(prevPoints => prevPoints + 1)
+                    }
+                    
+                }
+
+                setSubmitted(prevState => !prevState)
+            }
+
+// https://bobbyhadz.com/blog/react-change-style-on-click
+
+            function handleClick(e) {
+            }
+            
+            const renderQuestions = questions.map((item,questionIndex) => {
                 const question = item.question
                 const answers = item.answers
                 return (
                     <section key={`question${questionIndex+1}`}>
                         <h2>{he.decode(question)}</h2>
-                        <div className="flex" >
+                        <div className="flex"  >
                             {answers.map((item,answerIndex) => {
                                 const answer = item.answer
                                 const correct = item.correct
+                                const myStyle = {
+                                    padding: '1em',
+                                    borderRadius: '10px',
+                                    backgroundColor: correct ? 'green' : 'red'
+                                }
                                 return (
                                     <label key={`answer${answerIndex+1}`}>
-                                    {correct.toString()}  
-                                    <p className="question-answer" data-answer={correct.toString()} >{he.decode(answer)}</p>
+                                    {/* {correct.toString()}   */}
+                                    {submitted ? <p className="question-answer" style={myStyle} >{he.decode(answer)} </p>
+                                     : <p className="question-answer" style={{padding: '1em'}} onClick={handleClick}>{he.decode(answer)} </p>}
                                     <input 
                                     className="hidden question-answer"
                                     type='radio'
                                     // name has to be the same name for the hook for the button to change
                                     name={`question${questionIndex+1}`}
                                     // set the value of the radio to a state
-                                    onClick={() => console.log(question,item)}
+                                    onClick={() =>  setCheckAnswer(currentState => ({
+                                        ...currentState, [question] : answer
+                                    }))}
                                     // onChange={handleChange}
                                     value={correct}/>
                                 </label>
@@ -60,6 +99,7 @@ export default function Questions(props){
         <div>
             {renderQuestions}
             {/* validate needs to check the answers and apply a background color to them */}
+            {submitted && <p>you got {points} out of the {questions.length} questions</p>}
             <button onClick={validate}>{!submitted ? 'Submit' : 'Restart'}</button>
         </div>
     )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,56 +3,31 @@ import Questions from "./Questions";
 import he from "he"
 
 export default function Quiz(){
+    
     const [questions, setQuestions] = React.useState({})
     const [load, setLoad] = React.useState(false)
-    const [answers, setAnswers] = React.useState({
-        question1 : '',
-        question2 : '',
-        question3 : '',
-        question4 : '',
-        question5 : ''
-    })
-
-    const [newQuestion, setNewQuestion] = React.useState({
-        question : '',
-        answers : []
-    })
-    
-    React.useEffect(() => {
-        console.log(answers)
-    },[answers])
     
     React.useEffect(() => {
         apiCall()
     },[])
     
+    
+
     async function apiCall(){
         setLoad(false)
-        const response = await fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+        // normal api https://opentdb.com/api.php?amount=5&type=multiple
+        const response = await fetch('https://opentdb.com/api.php?amount=5&category=29&type=multiple&')
         const data = await response.json()
         const questions = data.results
-        console.log(questions)
 
         const updatedQuestions = await questions.map(item => {
             return {...item, 'answers' : shuffle(answerArray(item))}
         })
+        // console.log(updatedQuestions)
         setQuestions(updatedQuestions)
         setLoad(true)
+        return questions
     }
-
-    // function answerArray(array){
-    //     array.map(question => {
-    //         const wrongAnswers = question.incorrect_answers
-    //         const wrongAnswersObj = wrongAnswers.map(answer => {
-    //             return {'answer': answer, 'correct': false}
-    //         })
-    //         const correctAnswer = question.correct_answer
-    //         const correctAnswerObj = {'answer': correctAnswer, 'correct': true}
-    //         const questionList = [...wrongAnswersObj, correctAnswerObj]
-    //         console.log(questionList)
-    //         return questionList
-    //     })
-    // }
 
     function answerArray(obj){
        const incorrectAnswers = obj.incorrect_answers.map(answer => {
@@ -92,61 +67,21 @@ export default function Quiz(){
         })
     }
 
-//    function validate(e){
-//         e.preventDefault()
-//         console.log(answers)
-//         document.querySelectorAll('.question-answer').forEach(answer => {
-            
-//             // console.log(answer.getAttribute('data-answer'))
-//             // if (answer.data-answer === 'true'){
-//             //     answer.setAttribute('class', 'green')
-//             // }
-//         })
-//         // if (answers.question1 === 'true'){
-            
-//         // }
-   
-//     }
+   function validate(e){
+        e.preventDefault()
+        setSubmitted(prevState => !prevState)   
+    }
  
-    console.log('answers',questions)
     return (
 // make this into its own component with props
         <div>
             <h1>Quizzical</h1>
-            <form>
+            <form >
                 {/* if load is true map out the question array and render each question with a random order of answer choices */}
-                {load && <Questions questions={questions} />}
-                {/* {load && questions.map((item, questionIndex) => {
-                    const question = item.question
-                    const answers = item.answers
-                    return (
-                        <section key={`question${questionIndex+1}`}>
-                            <h2>{he.decode(question)}</h2>
-                            <div className="flex">
-                            {answers.map((item, answerIndex) => {
-                                const answer = item.answer
-                                const correct = item.correct
-                                return (
-                                    <label key={`answer${answerIndex+1}`}>
-                                     {correct.toString()}  
-                                    <p className="question-answer" data-answer={correct.toString()} >{he.decode(answer)}</p>
-                                    <input 
-                                    className="hidden question-answer"
-                                    type='radio'
-                                    // name has to be the same name for the hook for the button to change
-                                    name={`question${questionIndex+1}`}
-                                    // onClick={() => console.log(question,item)}
-                                    onChange={handleChange}
-                                    value={correct}/>
-                                    </label>
-                                )
-                            })}
-                            </div>
-                            <hr/>
-                        </section>
-                    ) 
-                })} */}
-                {/* <button onClick={validate}>Submit</button> */}
+                {load && <Questions questions={questions} apiCall={() => apiCall} />}
+              
+                    {/* <button onClick={validate} >{!submitted ? 'Submit' : 'Restart'}</button> */}
+
             </form>    
         </div>
         )
